7bba20b408fc06daae2235aa7f7d1b17fdde0288
Maikel <Maikel_J@Live.nl>
2018-01-06 16:48:07 +0100

    Added testing for all user crud operations

--- test/controllers/user_controller_test.js
+++ test/controllers/user_controller_test.js
@@ -3,6 +3,7 @@ const chai_http = require('chai-http');
 const server = require('../../index');
 const expect = chai.expect;
 const bcrypt = require('bcryptjs');
+const auth = require('../../auth/token');
 
 const User = require('../../models/user');
 
@@ -13,8 +14,8 @@ describe('User registration', () => {
         email: 'test@test11.com',
         password: 'test1234',
         firstname: '22131tester1,',
-        lastname: 'testing' ,
-        birth: 1993-6-24,
+        lastname: 'testing',
+        birth: 1993 - 6 - 24,
         gender: 'male',
         address: {
             street: 'Hinderstraat',
@@ -36,7 +37,7 @@ describe('User registration', () => {
             .end((err, res) => {
                 expect(err).to.be.null;
                 expect(res).to.have.status(201);
-                expect(res.body).to.include({ "msg": "User successfully created"});
+                expect(res.body).to.include({"msg": "User successfully created"});
                 done();
             });
     });
@@ -50,7 +51,7 @@ describe('User registration', () => {
                 .end((err, res) => {
                     expect(err).to.not.be.null;
                     expect(res).to.have.status(409);
-                    expect(res.body).to.include({ error: "User already exists"});
+                    expect(res.body).to.include({error: "User already exists"});
                     done();
                 });
         });
@@ -68,13 +69,13 @@ describe('User registration', () => {
     })
 });
 
-describe('Get all Users', () => {
-    let credentials = {
-        email: 'test@test11.com',
+describe('User login', () => {
+    const credentials = {
+        email: 'test@test.com',
         password: 'test1234',
         firstname: '22131tester1,',
-        lastname: 'testing' ,
-        birth: 1993-6-24,
+        lastname: 'testing',
+        birth: 1993 - 6 - 24,
         gender: 'male',
         address: {
             street: 'Hinderstraat',
@@ -89,10 +90,253 @@ describe('Get all Users', () => {
         }
     }
 
-    it('Get to /api/users/:id?', (done) => {
-        chai.request(server)
-            .post('/api/v1/reqister')
-            .send(credentials)
-    })
+    beforeEach((done) => {
+        const testUser = new User({
+            email: 'test@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
 
+        testUser.save().then(() => done());
+    });
+
+    it('Valid login', (done) => {
+        chai.request(server)
+            .post('/api/v1/login')
+            .send(credentials)
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Invalid password', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .send({email: credentials.email, password: 'test'})
+            .end((err, res) => {
+                expect(err).to.not.be.null;
+                expect(res).to.have.status(401);
+                expect(res.body).to.include({"error": "Invalid password"});
+                done();
+            });
+    });
+
+    it('Invalid email', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .send({email: "invalid@hotmail.com", password: credentials.password})
+            .end((err, res) => {
+                expect(err).to.not.be.null;
+                expect(res).to.have.status(404);
+                expect(res.body).to.include({"error": "User not found"});
+                done();
+            });
+    });
+
+    it('No email and password', function (done) {
+        chai.request(server)
+            .post('/api/v1/login')
+            .end((err, res) => {
+                expect(err).to.not.be.null;
+                expect(res).to.have.status(400);
+                expect(res.body).to.include({error: "Invalid Login Credentials"});
+                done();
+            });
+    });
+});
+
+describe('Retrieving user', () => {
+
+    it('Retrieves a single user', (done) => {
+        const testUser = new User({
+            email: 'test@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
+
+        User.create(testUser)
+            .then((userDb) => {
+                auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+                    chai.request(server)
+                        .get(`/api/v1/users/${userDb._id}`)
+                        .set({Authorization: `Bearer ${accessToken}`})
+                        .end((err, res) => {
+                            expect(err).to.be.null;
+                            expect(res).to.have.status(200);
+                            expect(res.body).to.be.a('object');
+                            expect(res.body).to.include({email: 'test@test.com'});
+                            done();
+                        });
+                });
+            });
+    });
+
+    it('Retrieving multiple users', (done) => {
+        const testUser1 = new User({
+            email: 'test@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
+
+        const testUser2 = new User({
+            email: 'SecondTestEmail@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
+
+        User.create(testUser1).then(() => {
+            User.create(testUser2)
+                .then((userDb) => {
+                    auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+                        chai.request(server)
+                            .get(`/api/v1/users/`)
+                            .set({Authorization: `Bearer ${accessToken}`})
+                            .end((err, res) => {
+                                expect(err).to.be.null;
+                                expect(res).to.have.status(200);
+                                expect(res.body).to.be.a('array').and.have.lengthOf(2);
+                                expect(res.body[1]).to.include({email: `${testUser2.email}`});
+                                done();
+                            });
+                    });
+                });
+        });
+    });
+});
+
+describe('Modifying user', () => {
+
+    it('Updating existing user', (done) => {
+        const testUser = new User({
+            email: 'test@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
+
+        User.create(testUser)
+            .then((userDb) => {
+                auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+                    userDb.firstname = "UpdatedName";
+                    chai.request(server)
+                        .put(`/api/v1/users/${userDb._id}`)
+                        .send(userDb)
+                        .set({Authorization: `Bearer ${accessToken}`})
+                        .end((err, res) => {
+                            expect(err).to.be.null;
+                            expect(res).to.have.status(202);
+                            expect(res.body).to.include({firstname: 'UpdatedName'});
+                            done();
+                        });
+                });
+            });
+    });
+
+    it('Deleting existing user', (done) => {
+        const testUser = new User({
+            email: 'test@test.com',
+            password: bcrypt.hashSync('test1234'),
+            firstname: '22131tester1,',
+            lastname: 'testing',
+            birth: 1993 - 6 - 24,
+            gender: 'male',
+            address: {
+                street: 'Hinderstraat',
+                number: 1,
+                postal_code: '3077DA',
+                city: 'Rotterdam',
+                state: 'Zuid-Holland',
+                country: 'Nederland',
+                geometry: {
+                    coordinates: [4.567827, 51.886838]
+                }
+            }
+        });
+
+        User.create(testUser)
+            .then((userDb) => {
+                auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+                    chai.request(server)
+                        .delete(`/api/v1/users/${userDb._id}`)
+                        .send(userDb)
+                        .set({Authorization: `Bearer ${accessToken}`})
+                        .end((err, res) => {
+                            expect(err).to.be.null;
+                            expect(res).to.have.status(200);
+                            done();
+                        });
+                });
+            });
+    });
 });
