a3e07293c1af2b25d667f66a0fe79343ac03da21
Thimo Koolen <tkoolen1@student.avans.nl>
2018-01-06 14:00:40 +0100

    Mistake

--- db/neo4j.js
+++ db/neo4j.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
-const config = require('../config/env/env.js');
+const config = require('../config/env.js');
 
 const db = new neo4j.driver(config.neo4j.url, neo4j.auth.basic(config.neo4j.username, config.neo4j.password));
 
