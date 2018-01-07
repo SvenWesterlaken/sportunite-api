19ca187f8cd1ee5f8cc66bb370ef9278d37070c7
Twan van Maastricht <ajmaastr@avans.nl>
2018-01-07 16:25:27 +0100

    Create tests for user registration, login, authentication and update, read, delete...made some different changes than Maikel.

--- controllers/user.js
+++ controllers/user.js
@@ -42,93 +42,93 @@ register(req, res, next) {
 },
 
 login(req, res, next) {
-let email = req.body.email || '';
-let password = req.body.password || '';
+    let email = req.body.email || '';
+    let password = req.body.password || '';
 
-if (email != '' || password != '') {
+    if (email != '' || password != '') {
 
-  User.findOne({email: email}).catch((err) => next(err)).then((user) => {
+      User.findOne({email: email}).catch((err) => next(err)).then((user) => {
 
-    if (user) {
-      bcrypt.compare(password, user.password).catch((err) => next(err)).then((valid) => {
+        if (user) {
+          bcrypt.compare(password, user.password).catch((err) => next(err)).then((valid) => {
 
-        if (valid) {
-          var token = auth.encodeToken(user).catch((err) => next(err)).then((token) => {
-            res.status(200).json({token: token});
+            if (valid) {
+              var token = auth.encodeToken(user).catch((err) => next(err)).then((token) => {
+                res.status(200).json({token: token});
+              });
+            } else {
+              res.status(401).json({error: "Invalid password"});
+            }
+
           });
+
         } else {
-          res.status(401).json({error: "Invalid password"});
+          res.status(404).json({error: "User not found"});
         }
 
       });
 
     } else {
-      res.status(404).json({error: "User not found"});
+      res.status(400).json({error: "Invalid Login Credentials"});
     }
-
-  });
-
-} else {
-  res.status(400).json({error: "Invalid Login Credentials"});
-}
 },
 
 //Reading a user
 read(req, res, next) {
 const userId = req.params.id || '';
 
-if (userId !== '') {
-  if (objectId.isValid(userId)) {
-    User.findById(userId).then((user) => {
-      if (user) {
-        res.status(200).send(user)
+    if (userId !== '') {
+      if (objectId.isValid(userId)) {
+        User.findById(userId).then((user) => {
+          if (user) {
+            res.status(200).send(user)
+          } else {
+            res.status(204).json({error: "User not found"});
+          }
+        }).catch((err) => next(err));
       } else {
-        res.status(204).json({error: "User not found"});
+        res.status(422).json({error: "Invalid user id"});
       }
-    }).catch((err) => next(err));
-  } else {
-    res.status(422).json({error: "Invalid user id"});
-  }
-} else {
-  User.find({}).then((users) => res.send(users)).catch((err) => next(err));
-}
+    } else {
+      User.find({}).then((users) => res.status(200).send(users)).catch((err) => next(err));
+    }
 },
 
 //Updating a user
 update(req, res, next) {
-const userId = req.params.id;
-const user = req.body;
+    const userId = req.params.id;
+    const user = req.body;
 
-if (objectId.isValid(userId) && (user._id === undefined || user._id === userId)) {
-  User.findByIdAndUpdate(userId, user)
-    .then((userDb) => {
-      if (userDb) {
-        User.findById(userId).then(user => res.status(202).send(user)).catch(next);
-      } else {
-        res.status(204).json({error: "User not found"});
-      }
-    })
-} else {
-  res.status(422).json({error: "Invalid user id"});
-}
+    if (objectId.isValid(userId) && (user._id === undefined || user._id === userId)) {
+      User.findByIdAndUpdate(userId, user)
+        .then((userDb) => {
+          if (userDb) {
+            User.findById(userId).then(user => res.status(202).send(user)).catch(next);
+          } else {
+            res.status(204).json({error: "User not found"});
+          }
+        })
+    } else {
+      res.status(422).json({error: "Invalid user id"});
+    }
 },
 
 //Deleting a user
 delete(req, res, next) {
-const userId = req.params.id;
+    const userId = req.params.id;
 
-if (objectId.isValid(userId)) {
-  User.findByIdAndRemove(userId)
-    .then((userDb) => {
-      if (userDb) {
-        res.status(200).send(userDb);
-      } else {
-        res.status(204).json({error: "User not found"});
-      }
-    }).catch((err) => next(err));
-} else {
-  res.status(422).json({error: "Invalid user id"});
-}
+    if (objectId.isValid(userId)) {
+      User.findByIdAndRemove(userId)
+        .then((userDb) => {
+          if (userDb) {
+            res.status(200).send(userDb);
+          } else {
+            res.status(204).json({error: "User not found"});
+          }
+        }).catch((err) => next(err));
+    } else {
+      res.status(422).json({error: "Invalid user id"});
+    }
 }
 
 };
--- test/controllers/user_controller_test.js
+++ test/controllers/user_controller_test.js
@@ -2,7 +2,9 @@ const chai = require('chai');
 const chai_http = require('chai-http');
 const server = require('../../index');
 const expect = chai.expect;
+const assert = chai.assert;
 const bcrypt = require('bcryptjs');
+const session = require('../../db/neo4j');
 
 const User = require('../../models/user');
 
@@ -27,7 +29,7 @@ describe('User registration', () => {
                 coordinates: [4.567827, 51.886838]
             }
         }
-    }
+    };
 
     it('Valid registration', (done) => {
         chai.request(server)
@@ -37,7 +39,23 @@ describe('User registration', () => {
                 expect(err).to.be.null;
                 expect(res).to.have.status(201);
                 expect(res.body).to.include({ "msg": "User successfully created"});
-                done();
+
+                User.findOne({ email: 'test@test11.com' })
+                    .then((user) => {
+                        assert(user.firstname === '22131tester1,');
+
+                        session
+                            .run(
+                                "MATCH (user:User) RETURN user"
+                            )
+                            .then((result) => {
+                               assert(result.records.length === 1);
+                               session.close();
+                               done();
+                            });
+                    });
+
+
             });
     });
 
@@ -68,8 +86,9 @@ describe('User registration', () => {
     })
 });
 
-describe('Get all Users', () => {
-    let credentials = {
+describe('User login', () => {
+
+    const credentials = {
         email: 'test@test11.com',
         password: 'test1234',
         firstname: '22131tester1,',
@@ -87,12 +106,251 @@ describe('Get all Users', () => {
                 coordinates: [4.567827, 51.886838]
             }
         }
-    }
+    };
 
-    it('Get to /api/users/:id?', (done) => {
+    const user = {
+        email: credentials.email,
+        password: credentials.password
+    };
+
+    const userWithInvalidEmail = {
+        email: 'invalidemail@hotmail.com',
+        password: credentials.password
+    };
+
+    const userWithInvalidPassword = {
+        email: credentials.email,
+        password: 'InvalidPassword'
+    };
+
+    it('Successful login', (done) => {
         chai.request(server)
-            .post('/api/v1/reqister')
+            .post('/api/v1/register')
             .send(credentials)
-    })
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(user)
+                    .end((err, res) => {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('token');
+                        done();
+                    })
+
+            });
+
+    });
+
+    it('Login fails due to invalid email', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(userWithInvalidEmail)
+                    .end((err, res) => {
+                        expect(res).to.have.status(404);
+                        expect(res.body).to.include({error: "User not found"});
+                        done();
+                    })
+
+            });
+    });
+
+    it('Login fails due to invalid password', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(userWithInvalidPassword)
+                    .end((err, res) => {
+                        expect(res).to.have.status(401);
+                        expect(res.body).to.include({error: "Invalid password"});
+                        done();
+                    })
+
+            });
+    });
+});
+
+describe('Read, Update and Delete on user', () => {
+    let credentials1 = {
+        email: 'test1@test11.com',
+        password: 'test1234',
+        firstname: 'tester1',
+        lastname: 'testing' ,
+        birth: 1993-6-24,
+        gender: 'male',
+        address: {
+            street: 'Hinderstraat',
+            number: 1,
+            postal_code: '3077DA',
+            city: 'Rotterdam',
+            state: 'Zuid-Holland',
+            country: 'Nederland',
+            geometry: {
+                coordinates: [4.567827, 51.886838]
+            }
+        }
+    };
+
+    let credentials2 = {
+        email: 'test2@test11.com',
+        password: 'test1234',
+        firstname: 'tester2',
+        lastname: 'testing' ,
+        birth: 1993-6-24,
+        gender: 'male',
+        address: {
+            street: 'Hinderstraat',
+            number: 1,
+            postal_code: '3077DA',
+            city: 'Rotterdam',
+            state: 'Zuid-Holland',
+            country: 'Nederland',
+            geometry: {
+                coordinates: [4.567827, 51.886838]
+            }
+        }
+    };
+
+    let user = {
+        email: credentials1.email,
+        password: credentials1.password
+    };
+
+    it('Get to /api/v1/users/:id? returns all users', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials1)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/register')
+                    .send(credentials2)
+                    .end((err, res) => {
+                        chai.request(server)
+                            .post('/api/v1/login')
+                            .send(user)
+                            .end((err, res) => {
+                                chai.request(server)
+                                    .get('/api/v1/users')
+                                    .set('Authorization', 'Bearer ' + res.body.token)
+                                    .end((err, res) => {
+                                        expect(err).to.be.null;
+                                        expect(res).to.have.status(200);
+                                        expect(res.body).to.be.an('array');
+                                        expect(res.body[0]).to.deep.include({email: 'test1@test11.com'});
+                                        expect(res.body[1]).to.deep.include({email: 'test2@test11.com'});
+                                        done();
+                                    })
+                            });
+
+
+                    })
+            })
+    });
+
+    it('Get to /api/v1/users/:id? returns one user with specific a specific id', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials1)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(user)
+                    .end((err, res) => {
+                        User.findOne({email: 'test1@test11.com'})
+                            .then((user) => {
+                                chai.request(server)
+                                    .get(`/api/v1/users/${user._id}`)
+                                    .set('Authorization', 'Bearer ' + res.body.token)
+                                    .end((err, res) => {
+                                        expect(err).to.be.null;
+                                        expect(res).to.have.status(200);
+                                        expect(res.body).to.deep.include({email: 'test1@test11.com'});
+                                        done();
+                                    })
+                            })
+
+                    });
+
+            })
+    });
+
+    it('Put to /api/v1/users/:id updates a user', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials1)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(user)
+                    .end((err, res) => {
+                        User.findOne({email: 'test1@test11.com'})
+                            .then((user) => {
+                                chai.request(server)
+                                    .put(`/api/v1/users/${user._id}`)
+                                    .send({gender: 'female'})
+                                    .set('Authorization', 'Bearer ' + res.body.token)
+                                    .end((err, res) => {
+                                        expect(err).to.be.null;
+                                        expect(res).to.have.status(202);
+                                        expect(res.body).to.deep.include({email: 'test1@test11.com'});
+                                        expect(res.body).to.deep.include({gender: 'female'});
+                                        done();
+                                    })
+                            })
+
+                    });
+
+            })
+    });
+
+    it.only('Delete to /api/v1/users/:id deletes a user', (done) => {
+        chai.request(server)
+            .post('/api/v1/register')
+            .send(credentials1)
+            .end((err, res) => {
+                chai.request(server)
+                    .post('/api/v1/login')
+                    .send(user)
+                    .end((err, res) => {
+                        User.findOne({email: 'test1@test11.com'})
+                            .then((user) => {
+                                chai.request(server)
+                                    .delete(`/api/v1/users/${user._id}`)
+                                    .set('Authorization', 'Bearer ' + res.body.token)
+                                    .end((err, res) => {
+                                        expect(err).to.be.null;
+                                        expect(res).to.have.status(200);
+
+                                        const userId = res.body._id;
+
+                                        User.findOne({email: 'test1@test11.com'})
+                                            .then((user) => {
+                                                assert(user === null);
+                                                session
+                                                    .run(
+                                                        "MATCH (user:User) RETURN user"
+                                                    )
+                                                    .then((result) => {
+                                                        console.log('result: ' + JSON.stringify(result.records.length));
+                                                        assert(result.records.length === 0);
+                                                        session.close();
+                                                        done();
+                                                    });
+                                            });
+
+                                    })
+                            })
+
+                    });
+
+            })
+    });
 
 });
