f30ece186920e4199c95c872885092eeb4619bb5
Thimo Koolen <tkoolen1@student.avans.nl>
2018-01-11 15:42:53 +0100

    Created endpoint test for event leave

--- test/controllers/sportevent_controller_test.js
+++ test/controllers/sportevent_controller_test.js
@@ -9,56 +9,56 @@ const auth = require('../../auth/token');
 const User = require('../../models/user');
 
 describe('Add Sportevent', () => {
-    it.only('add a Sportevent', (done) => {
-        const userTest = new User({
-            email: 'test@test.com',
-            password: bcrypt.hashSync('test1234'),
-            firstname: '22131tester1,',
-            lastname: 'testing',
-            birth: 1993 - 6 - 24,
-            gender: 'male',
-            address: {
-                street: 'Hinderstraat',
-                number: 1,
-                postal_code: '3077DA',
-                city: 'Rotterdam',
-                state: 'Zuid-Holland',
-                country: 'Nederland',
-                geometry: {
-                    coordinates: [4.567827, 51.886838]
-                }
-            }
-        });
-        const sportEventId = 12;
-
-        User.create(userTest)
-            .then((userDb) => {
-            auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
-                session.run(`CREATE (e:Event{id: ${sportEventId}}) RETURN e;`)
-                    .then((result1) => {
-                        session.run(`CREATE (u:User {id: "${userDb._id}"}) RETURN u;`)
-                            .then((result2) => {
-                                chai.request(server)
-                                    .post(`/api/v1/sportevents/`)
-                                    .send({email: userTest.email, eventId: sportEventId})
-                                    .set({Authorization: `Bearer ${accessToken}`})
-                                    .end((err, res) => {
-                                        session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH(u)<-[:CREATED_BY]-(e) RETURN u,e;`)
-                                            .then((result3) => {
-                                                console.log(result3)
-                                                expect(err).to.be.null;
-                                                expect(res).to.have.status(201);
-                                                expect(res.body).to.include({msg: "Event successfully created"});
-                                                expect(result3.records[0]._fields[0].labels[0]).to.be.equal('User');
-                                                expect(result3.records[0]._fields[1].labels[0]).to.be.equal('Event');
-                                                done();
-                                            });
-                                    });
-                            });
-                    })
-                })
-            })
-    })
+	it('add a Sportevent', (done) => {
+		const userTest = new User({
+			email: 'test@test.com',
+			password: bcrypt.hashSync('test1234'),
+			firstname: '22131tester1,',
+			lastname: 'testing',
+			birth: 1993 - 6 - 24,
+			gender: 'male',
+			address: {
+				street: 'Hinderstraat',
+				number: 1,
+				postal_code: '3077DA',
+				city: 'Rotterdam',
+				state: 'Zuid-Holland',
+				country: 'Nederland',
+				geometry: {
+					coordinates: [4.567827, 51.886838]
+				}
+			}
+		});
+		const sportEventId = 12;
+		
+		User.create(userTest)
+			.then((userDb) => {
+				auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+					session.run(`CREATE (e:Event{id: ${sportEventId}}) RETURN e;`)
+						.then((result1) => {
+							session.run(`CREATE (u:User {id: "${userDb._id}"}) RETURN u;`)
+								.then((result2) => {
+									chai.request(server)
+										.post(`/api/v1/sportevents/`)
+										.send({email: userTest.email, eventId: sportEventId})
+										.set({Authorization: `Bearer ${accessToken}`})
+										.end((err, res) => {
+											session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH(u)<-[:CREATED_BY]-(e) RETURN u,e;`)
+												.then((result3) => {
+													console.log(result3)
+													expect(err).to.be.null;
+													expect(res).to.have.status(201);
+													expect(res.body).to.include({msg: "Event successfully created"});
+													expect(result3.records[0]._fields[0].labels[0]).to.be.equal('User');
+													expect(result3.records[0]._fields[1].labels[0]).to.be.equal('Event');
+													done();
+												});
+										});
+								});
+						})
+				})
+			})
+	})
 })
 
 describe('Attend Sportevent', () => {
@@ -105,6 +105,60 @@ describe('Attend Sportevent', () => {
 													expect(neoresult3.records[0]._fields[0].labels[0]).to.be.equal('User');
 													expect(neoresult3.records[0]._fields[1].labels[0]).to.be.equal('Event');
 													done();
+												});
+										});
+								});
+						});
+				});
+			});
+	});
+});
+
+describe('Leave Sportevent', () => {
+	it('leave a sportevent', (done) => {
+		const testUser2 = new User({
+			email: 'test@test.com',
+			password: bcrypt.hashSync('test1234'),
+			firstname: '22131tester1,',
+			lastname: 'testing',
+			birth: 1993 - 6 - 24,
+			gender: 'male',
+			address: {
+				street: 'Hinderstraat',
+				number: 1,
+				postal_code: '3077DA',
+				city: 'Rotterdam',
+				state: 'Zuid-Holland',
+				country: 'Nederland',
+				geometry: {
+					coordinates: [4.567827, 51.886838]
+				}
+			}
+		});
+		
+		const sportEventId = 1111;
+		
+		User.create(testUser2)
+			.then((userDb) => {
+				auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+					session.run(`CREATE (e:Event{id: ${sportEventId}}) RETURN e;`)
+						.then((neoresult1) => {
+							session.run(`CREATE (u:User {id: "${userDb._id}"}) RETURN u;`)
+								.then((neoresult2) => {
+									session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MERGE(u)-[:ATTENDS]->(e) RETURN u,e;`)
+										.then((neoresult3) => {
+											chai.request(server)
+												.post(`/api/v1/sportevents/${sportEventId}/leave`)
+												.send({email: testUser2.email, eventId: sportEventId})
+												.set({Authorization: `Bearer ${accessToken}`})
+												.end((err, res) => {
+													session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH(u)-[r:ATTENDS]->(e) DELETE r;`)
+														.then((neoresult4) => {
+															expect(err).to.be.null;
+															expect(res).to.have.status(200);
+															expect(res.body).to.include({msg: "User succesfully removed from event"});
+															done();
+														});
 												});
 										});
 								});
