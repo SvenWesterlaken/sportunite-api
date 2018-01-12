1afb8adefb56765d92b83d09553b783a9b4ece50
Thimo Koolen <tkoolen1@student.avans.nl>
2018-01-12 14:14:10 +0100

    Fixed test

--- test/controllers/sportevent_controller_test.js
+++ test/controllers/sportevent_controller_test.js
@@ -154,10 +154,14 @@ describe('Leave Sportevent', () => {
 												.end((err, res) => {
 													session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH(u)-[r:ATTENDS]->(e) DELETE r;`)
 														.then((neoresult4) => {
-															expect(err).to.be.null;
-															expect(res).to.have.status(200);
-															expect(res.body).to.include({msg: "User succesfully removed from event"});
-															done();
+															session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH (e:Event{id: ${sportEventId}}) MATCH (u)-[:ATTENDS]->(e) RETURN u, e;`)
+																.then((neoresult5) => {
+																	expect(err).to.be.null;
+																	expect(res).to.have.status(200);
+																	expect(neoresult5.records).to.be.lengthOf(0);
+																	expect(res.body).to.include({msg: "User succesfully removed from event"});
+																	done();
+																});
 														});
 												});
 										});
@@ -169,56 +173,56 @@ describe('Leave Sportevent', () => {
 });
 
 describe('Delete Sportevent', () => {
-    it.only('delete a sportevent', (done) => {
-        const testingUser = new User({
-            email: 'test@test.com',
-            password: bcrypt.hashSync('test1234'),
-            firstname: '22131tester1,',
-            lastname: 'testing',
-            birth: 1993 - 6 - 24,
-            gender: 'male',
-            address: {
-                street: 'Hinderstraat',
-                number: 1,
-                postal_code: '3077DA',
-                city: 'Rotterdam',
-                state: 'Zuid-Holland',
-                country: 'Nederland',
-                geometry: {
-                    coordinates: [4.567827, 51.886838]
-                }
-            }
-        });
-
-        const sportEventId = 1111;
-
-        User.create(testingUser)
-            .then((userDb) => {
-                auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
-                    session.run(`CREATE (e:Event{id: ${sportEventId}}) CREATE (u:User{id:"${userDb._id}"}) RETURN e,u;`)
-                        .then((neoresult1) => {
-                            // console.log(neoresult1.records[0]._fields[1])
-                            session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MERGE (e)-[:CREATED_BY]->(u) RETURN e,u;`)
-                                .then((neoresult2) => {
-                                    // console.log(neoresult2.records[0]._fields[1])
-                                    chai.request(server)
-                                        .delete(`/api/v1/sportevents/${sportEventId}`)
-                                        .send({email: testingUser.email, eventId: sportEventId})
-                                        .set({Authorization: `Bearer ${accessToken}`})
-                                        .end((err, res) => {
-                                            session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH (e)-[:CREATED_BY]->(u) DETACH DELETE e RETURN u`)
-                                                .then((neoresult3) => {
-                                                    expect(err).to.be.null;
-                                                    expect(res).to.have.status(200);
-                                                    expect(res.body).to.include({msg: "Sport event successfully deleted"});
-                                                    session.close();
-                                                    done();
-                                                });
-                                        })
-
-                                });
-                        });
-                });
-            });
-    });
+	it('delete a sportevent', (done) => {
+		const testingUser = new User({
+			email: 'test@test.com',
+			password: bcrypt.hashSync('test1234'),
+			firstname: '22131tester1,',
+			lastname: 'testing',
+			birth: 1993 - 6 - 24,
+			gender: 'male',
+			address: {
+				street: 'Hinderstraat',
+				number: 1,
+				postal_code: '3077DA',
+				city: 'Rotterdam',
+				state: 'Zuid-Holland',
+				country: 'Nederland',
+				geometry: {
+					coordinates: [4.567827, 51.886838]
+				}
+			}
+		});
+		
+		const sportEventId = 1111;
+		
+		User.create(testingUser)
+			.then((userDb) => {
+				auth.encodeToken(userDb).catch((err) => next(err)).then((accessToken) => {
+					session.run(`CREATE (e:Event{id: ${sportEventId}}) CREATE (u:User{id:"${userDb._id}"}) RETURN e,u;`)
+						.then((neoresult1) => {
+							// console.log(neoresult1.records[0]._fields[1])
+							session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MERGE (e)-[:CREATED_BY]->(u) RETURN e,u;`)
+								.then((neoresult2) => {
+									// console.log(neoresult2.records[0]._fields[1])
+									chai.request(server)
+										.delete(`/api/v1/sportevents/${sportEventId}`)
+										.send({email: testingUser.email, eventId: sportEventId})
+										.set({Authorization: `Bearer ${accessToken}`})
+										.end((err, res) => {
+											session.run(`MATCH (u:User{id:"${userDb._id}"}) MATCH(e:Event{id: ${sportEventId}}) MATCH (e)-[:CREATED_BY]->(u) DETACH DELETE e RETURN u`)
+												.then((neoresult3) => {
+													expect(err).to.be.null;
+													expect(res).to.have.status(200);
+													expect(res.body).to.include({msg: "Sport event successfully deleted"});
+													session.close();
+													done();
+												});
+										})
+									
+								});
+						});
+				});
+			});
+	});
 });
